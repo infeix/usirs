5f2ad2e915a77b5b239721bf87ea48138bb46e0d
Johannes Kroop <johkroop@gmail.com>
2018-03-20 16:21:19 +0100

    ux

--- do.sh
+++ do.sh
@@ -1,74 +1,104 @@
 #!/bin/bash
 
-echo "Enter [DEPLOY_USER]:"
-read deploy_user
+# user input
+# ---
+echo "Enter [APP_NAME]:"
+read app_name
+db_user_name="${app_name}_user"
+echo "Enter [PASSWORD] for db user:"
+read db_user_pass
+deploy_user=`whoami`
+echo "Enter SECRET_KEY (bundle exec rake secret):"
+read secret_key
+app_dir=$app_name
 
-echo "Grand deploy passwordless sudo previleges (sudo needed) [ENTER]:"
-read notused
+user_action='ENTER'
+# ---
 
+echo "Grand deploy passwordless sudo previleges (sudo needed) [$user_action]:"
+if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
+if [ $notused = '!' ]; then user_action='CTRL + C' fi
 sudo bash -c "echo '$deploy_user ALL=NOPASSWD: ALL' >> /etc/sudoers"
 
-echo "Install apt-get packages (sudo needed) [ENTER]:"
-read notused
+# ---
+
+echo "Install apt-get packages (sudo needed) [$user_action]:"
+if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
+if [ $notused = '!' ]; then user_action='CTRL + C' fi
 
 echo "add the Passenger APT repository"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
 sudo apt-get install -y apt-transport-https ca-certificates
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main >
-/etc/apt/sources.list.d/passenger.list'
+sudo bash -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
+
+# ---
 
 echo "add yarn APT repository"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
+# ---
+
+echo "installation apt packages"
 sudo apt-get update
 sudo apt-get install -y nginx-extras passenger gnupg2 nodejs yarn postgresql-9.5 postgresql-server-dev-9.5
 
+# ---
 
-echo "Removing default nginx config [ENTER]:"
-read notused
+echo "Removing default nginx config [$user_action]:"
+if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
+if [ $notused = '!' ]; then user_action='CTRL + C' fi
 
 sudo rm /etc/nginx/sites-enabled/default
 
-echo "Enter [DB_USER_NAME]:"
-read db_user_name
+# ---
 
-echo "Enter db user [PASSWORD]:"
-read db_user_pass
+echo "Start creating Postgres User [$user_action]:"
+if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
+if [ $notused = '!' ]; then user_action='CTRL + C' fi
 
-echo "Start creating Postgres User [ENTER]:"
-read notused
 sudo -u postgres psql -c "CREATE ROLE $db_user_name PASSWORD '$db_user_pass' NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN;"
 sudo -u postgres psql -c "CREATE DATABASE $db_user_name OWNER $db_user_name"
-echo "Add GPG key for downloading rvm [ENTER]:"
-read notused
+
+# ---
+
+echo "Start download and install rvm [$user_action]:"
+# Add GPG key for downloading rvm
+if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
+if [ $notused = '!' ]; then user_action='CTRL + C' fi
+
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 gpg2 --recv-keys 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-echo "Start download and install rvm [ENTER]:"
-read notused
+# installation rvm
 \curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
-echo "Start install ruby (2.3.1) with rvm [ENTER]:"
-read notused
+# ---
+
+echo "Start install ruby (2.3.1) with rvm [$user_action]:"
+if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
+if [ $notused = '!' ]; then user_action='CTRL + C' fi
+
 rvm install 2.3.1
 rvm --default use 2.3.1
 rvm use 2.3.1
 
-echo "Start install bundler [ENTER]:"
-read notused
+# ---
+
+echo "Start install bundler [$user_action]:"
+if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
+if [ $notused = '!' ]; then user_action='CTRL + C' fi
+
 gem install bundler
 
-echo "Enter the app [DIRECTORY]:"
-read app_dir
+# ---
 
-echo "Enter secret key (bundle exec rake secret):"
-read secret_key
+echo "Start creating database.yml [$user_action]:"
+if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
+if [ $notused = '!' ]; then user_action='CTRL + C' fi
 
-echo "Start creating database.yml [ENTER]:"
-read notused
 mkdir ~/$app_dir
 mkdir ~/$app_dir/shared
 mkdir ~/$app_dir/shared/config
@@ -84,8 +114,10 @@ echo '  pool: 5' >> ~/$app_dir/shared/config/database.yml
 echo '  host: localhost' >> ~/$app_dir/shared/config/database.yml
 echo "  database: $db_user_name" >> ~/$app_dir/shared/config/database.yml
 
-echo "Start creating secrets.yml [ENTER]:"
-read notused
+echo "Start creating secrets.yml [$user_action]:"
+if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
+if [ $notused = '!' ]; then user_action='CTRL + C' fi
+
 echo "production:" >> ~/$app_dir/shared/config/secrets.yml
 echo "  secret_key_base: $secret_key" >> ~/$app_dir/shared/config/secrets.yml
 
