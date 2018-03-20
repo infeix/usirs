eafd163d6d23363adc738bc9560189ec65bdb812
Johannes Kroop <johkroop@gmail.com>
2018-03-20 16:30:04 +0100

    ux bug

--- do.sh
+++ do.sh
@@ -13,18 +13,28 @@ read secret_key
 app_dir=$app_name
 
 user_action='ENTER'
+
 # ---
 
-echo "Grand deploy passwordless sudo previleges (sudo needed) [$user_action]:"
-if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
-if [ $notused = '!' ]; then user_action='CTRL + C' fi
+function user_interact {
+  if [ $user_action = 'ENTER' ]; then
+    read notused
+  else
+    sleep 1
+  fi
+  if [ $notused = '!' ]; then
+    user_action='CTRL + C'
+  fi
+}
+
+echo "Grand deploy passwordless sudo previleges (sudo needed) [${user_action}]:"
+user_interact
 sudo bash -c "echo '$deploy_user ALL=NOPASSWD: ALL' >> /etc/sudoers"
 
 # ---
 
-echo "Install apt-get packages (sudo needed) [$user_action]:"
-if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
-if [ $notused = '!' ]; then user_action='CTRL + C' fi
+echo "Install apt-get packages (sudo needed) [${user_action}]:"
+user_interact
 
 echo "add the Passenger APT repository"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
@@ -46,28 +56,25 @@ sudo apt-get install -y nginx-extras passenger gnupg2 nodejs yarn postgresql-9.5
 
 # ---
 
-echo "Removing default nginx config [$user_action]:"
-if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
-if [ $notused = '!' ]; then user_action='CTRL + C' fi
+echo "Removing default nginx config [${user_action}]:"
+user_interact
 
 sudo rm /etc/nginx/sites-enabled/default
 
 # ---
 
-echo "Start creating Postgres User [$user_action]:"
-if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
-if [ $notused = '!' ]; then user_action='CTRL + C' fi
+echo "Start creating Postgres User [${user_action}]:"
+user_interact
 
 sudo -u postgres psql -c "CREATE ROLE $db_user_name PASSWORD '$db_user_pass' NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN;"
 sudo -u postgres psql -c "CREATE DATABASE $db_user_name OWNER $db_user_name"
 
 # ---
 
-echo "Start download and install rvm [$user_action]:"
-# Add GPG key for downloading rvm
-if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
-if [ $notused = '!' ]; then user_action='CTRL + C' fi
+echo "Start download and install rvm [${user_action}]:"
+user_interact
 
+# Add GPG key for downloading rvm
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 gpg2 --recv-keys 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
@@ -77,9 +84,8 @@ source ~/.rvm/scripts/rvm
 
 # ---
 
-echo "Start install ruby (2.3.1) with rvm [$user_action]:"
-if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
-if [ $notused = '!' ]; then user_action='CTRL + C' fi
+echo "Start install ruby (2.3.1) with rvm [${user_action}]:"
+user_interact
 
 rvm install 2.3.1
 rvm --default use 2.3.1
@@ -87,17 +93,15 @@ rvm use 2.3.1
 
 # ---
 
-echo "Start install bundler [$user_action]:"
-if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
-if [ $notused = '!' ]; then user_action='CTRL + C' fi
+echo "Start install bundler [${user_action}]:"
+user_interact
 
 gem install bundler
 
 # ---
 
-echo "Start creating database.yml [$user_action]:"
-if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
-if [ $notused = '!' ]; then user_action='CTRL + C' fi
+echo "Start creating database.yml [${user_action}]:"
+user_interact
 
 mkdir ~/$app_dir
 mkdir ~/$app_dir/shared
@@ -114,9 +118,8 @@ echo '  pool: 5' >> ~/$app_dir/shared/config/database.yml
 echo '  host: localhost' >> ~/$app_dir/shared/config/database.yml
 echo "  database: $db_user_name" >> ~/$app_dir/shared/config/database.yml
 
-echo "Start creating secrets.yml [$user_action]:"
-if [ $user_cation = 'ENTER' ];then read notused else sleep 1 fi
-if [ $notused = '!' ]; then user_action='CTRL + C' fi
+echo "Start creating secrets.yml [${user_action}]:"
+user_interact
 
 echo "production:" >> ~/$app_dir/shared/config/secrets.yml
 echo "  secret_key_base: $secret_key" >> ~/$app_dir/shared/config/secrets.yml
